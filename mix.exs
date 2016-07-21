e0cc1ab867b98eb526ea928c058e48e234beae0c
Dwight Watson <dwightwatson@me.com>
2016-07-21 13:22:55 +1000

    Install library for time_ago_in_words

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Lunchmates.Mixfile do
   def application do
     [mod: {Lunchmates, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :comeonin]]
+                    :phoenix_ecto, :postgrex, :comeonin, :phoenix_html_simplified_helpers]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,8 @@ defmodule Lunchmates.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.0"}]
+     {:comeonin, "~> 2.0"},
+     {:phoenix_html_simplified_helpers, "~> 0.6.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- web/web.ex
+++ web/web.ex
@@ -50,6 +50,7 @@ defmodule Lunchmates.Web do
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
+      use Phoenix.HTML.SimplifiedHelpers
 
       import Lunchmates.Router.Helpers
       import Lunchmates.ErrorHelpers
